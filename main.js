57696e3e6a154f9cf052110cde6c64e116747957
code-maker-kitty <77761014+code-maker-kitty@users.noreply.github.com>
2021-05-14 14:18:44 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,19 @@ recognition.onresult=function run(event){
     var Content=event.results[0][0].transcript;
     console.log(Content);
     document.getElementById("textbox").innerHTML=Content;
+    speak();
 }
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("textbox").value;
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'png',
+    png_quality: 90
+  });
+  Webcam.attach( "camera" );
